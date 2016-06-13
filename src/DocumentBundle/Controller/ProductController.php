fdabc80b2a798072503310277d55bb1567b757d8
Antoine <anton51200@laposte.net>
2016-06-13 16:24:06 +0200

    create controller/view for list and edit product

--- src/DocumentBundle/Controller/ProductController.php
+++ src/DocumentBundle/Controller/ProductController.php
@@ -19,11 +19,59 @@ class ProductController extends Controller
         $form = $this->createForm('DocumentBundle\Form\ProductType', $product);
         $form->handleRequest($request);
 
-        $em->persist($product);
-        $em->flush();
+        if ($form->isValid()) {
+            $em->persist($product);
+            $em->flush();
+        }
 
         return $this->render('default/newproduct.html.twig', array(
             'form' => $form->createView(),
         ));
     }
+
+    public function listeproductAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $products = $em->getRepository('DocumentBundle:Product')->findAll();
+
+        return $this->render('default/listeproduct.html.twig', array(
+            'products' => $products,
+        ));
+    }
+
+    public function editproductAction(Request $request, $idproduct)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $type = $request->request->get('type');
+        $nom = $request->request->get('nom');
+        $contenu = $request->request->get('contenu');
+
+        $hidden = $request->request->get('hidden');
+
+        $product = $em->getRepository('DocumentBundle:Product')->findOneById($idproduct);
+
+        if ($hidden == 1){
+            if (!empty($type)) {
+                $product->setType($type);
+            }
+            if (!empty($nom)) {
+                $product->setNom($nom);
+            }
+            if (!empty($contenu)) {
+                $product->setContenu($contenu);
+            }
+
+            $em->persist($product);
+            $em->flush();
+        }
+
+        return $this->render('default/editproduct.html.twig', array(
+            'product' => $product,
+        ));
+    }
+
 }
