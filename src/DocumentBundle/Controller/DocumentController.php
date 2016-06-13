888b1b5b59115503d8aab505ebf4dd96b8a67acd
FredGOVIGNON <fgovignon@gmail.com>
2016-06-13 12:05:31 +0200

    début CRUD DocumentController.php et routing

---
+++ src/DocumentBundle/Controller/DocumentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DocumentBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use DocumentBundle\Entity\Documents;
+
+class DocumentController extends Controller
+{
+    public function newdocumentAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+
+        return $this->render('default/newdoc.html.twig', array(
+            
+        ));
+    }
+}
