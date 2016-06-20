6f7cdb493ae5615db47d38f0ab6b7d5ef5b038f9
keny700 <fredericbouillere@yahoo.fr>
2016-06-20 09:50:05 +0200

    pdf

--- src/DocumentBundle/Controller/DocumentController.php
+++ src/DocumentBundle/Controller/DocumentController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use DocumentBundle\Entity\Documents;
+use Html2Pdf_Html2Pdf;
+
+
 
 class DocumentController extends Controller
 {
@@ -20,10 +23,11 @@ class DocumentController extends Controller
         ));
     }
 
-public function newdevisAction(Request $request)
+    public function newdevisAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $user = $this->container->get('security.context')->getToken()->getUser();
+
 
 
         return $this->render('default/newdevis.html.twig', array(
@@ -31,7 +35,32 @@ public function newdevisAction(Request $request)
         ));
     }
 
-
+    public function pdfAction()
+    {
+        //on stocke la vue à convertir en PDF, en n'oubliant pas les paramètres twig si la vue comporte des données dynamiques
+        $html = $this->renderView('default/newpdf.html.twig'/*, array('name' => $name)*/);
+         
+        //on instancie la classe Html2Pdf_Html2Pdf en lui passant en paramètre
+        //le sens de la page "portrait" => p ou "paysage" => l
+        //le format A4,A5...
+        //la langue du document fr,en,it...
+        $html2pdf = new \Html2Pdf_Html2Pdf('P','A4','fr');
+ 
+        //SetDisplayMode définit la manière dont le document PDF va être affiché par l’utilisateur
+        //fullpage : affiche la page entière sur l'écran
+        //fullwidth : utilise la largeur maximum de la fenêtre
+        //real : utilise la taille réelle
+        $html2pdf->pdf->SetDisplayMode('fullpage');
+ 
+        //writeHTML va tout simplement prendre la vue stocker dans la variable $html pour la convertir en format PDF
+        $html2pdf->writeHTML($html);
+ 
+        //Output envoit le document PDF au navigateur internet avec un nom spécifique qui aura un rapport avec le contenu à convertir (exemple : Facture, Règlement…)
+        $html2pdf->Output('devis.pdf');
+         
+     
+        return new Response();
+    }
 
 
 }
