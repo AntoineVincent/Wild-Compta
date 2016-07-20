07d3ddc50ce56d01355f081264d1a3f6c5fa1c2a
Antoine <anton51200@laposte.net>
2016-07-20 09:36:08 +0200

    merge

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use ComptaBundle\Entity\Reglement;
 
 class DashboardController extends Controller
 {
@@ -13,9 +14,20 @@ class DashboardController extends Controller
      */
     public function dashboardAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $ca = "";
+
+        $reglements = $em->getRepository('ComptaBundle:Reglement')->findAll();
+
+        foreach ($reglements as $reglement) {
+            $value = $reglement->getMontant();
+            $ca += $value;
+        }
+
         return $this->render('default/dashboard.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'ca' => $ca,
         ));
     }
 }
