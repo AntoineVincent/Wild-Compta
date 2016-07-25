fbc30adc492cb42d9ad21a1de9c0e9e07cb618ba
keny700 <fredericbouillere@yahoo.fr>
2016-07-25 15:29:36 +0200

    redirections

---
+++ src/DocumentBundle/Form/FactureType.php
@@ -0,0 +1,38 @@
+<?php
+
+// src/DocumentBundle/Form/FactureType.php
+namespace DocumentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use DocumentBundle\Entity\Documents;
+use ClientBundle\Entity\Client;
+
+class FactureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            /*->add('nom', EntityType::class, array(
+                'class' => 'ClientBundle:Client'
+            ))
+            ->add('adresse', EntityType::class, array(
+                'class' => 'ClientBundle:Client'
+            ))*/
+            ->add('type')
+            ->add('reference')
+            ->add('datecreation')
+            ->add('tva');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'DocumentBundle\Entity\Documents',
+                'ClientBundle\Entity\Client'
+        ));
+    }
+}
