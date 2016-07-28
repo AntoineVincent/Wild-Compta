46b8b6aca4a263f6d1377f9745b9b789f5bea873
Antoine <anton51200@laposte.net>
2016-07-28 14:41:15 +0200

    update ajax function for ca ville

--- src/AppBundle/Controller/DashboardController.php
+++ src/AppBundle/Controller/DashboardController.php
@@ -8,6 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 use ComptaBundle\Entity\Reglement;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use ClientBundle\Entity\Ecole;
 
 class DashboardController extends Controller
 {
@@ -22,14 +29,12 @@ class DashboardController extends Controller
         $ecoles = $em->getRepository('ClientBundle:Ecole')->findAll();
         $ecolerslt = $request->request->get('ecole');
 
-
+        // DECLARATION VARIABLES CA ET CA HORS TAXE ANNUEL
         $ca = "";
         $caht = "";
         
-
+        // CALCUL POUR CHAQUE REGLEMENT DU CA ET DU CA HT A L'ANNEE
         $reglements = $em->getRepository('ComptaBundle:Reglement')->findAll();
-        
-
         foreach ($reglements as $reglement) {
             $value = $reglement->getMontant();
             $ca += $value;
@@ -38,14 +43,13 @@ class DashboardController extends Controller
             $caht = number_format((float)($caht), 2, ',', ' ');    
         }
         
+        //DACLERATION VARABLES CA PAR MOIS
         $camois = "";
         $camoisht = "";
-    
+        // CALCUL POUR CHAQUE REGLEMENT DU CA ET DU CA HT AU MOIS EN COURS
         $datemois = new \DateTime();
-
         $reglementsmois = $em->getRepository('ComptaBundle:Reglement')->findByDatemois($datemois->format('m/Y'));
         
-
         foreach ($reglementsmois as $reglementmois) {
             $valuemois = $reglementmois->getMontant();
             $camois += $valuemois;
@@ -61,5 +65,57 @@ class DashboardController extends Controller
             'camoisht' => $camoisht,
             'ecoles' => $ecoles
         ));
+    }
+    /**
+     * @Route("/dashboard/cabyecole", name="requestajax")
+     */
+    public function cabyecole(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $caville = 0;
+        $cavilleht = 0;
+        $tabcaville = [];
+        $ecole = $this->getRequest()->request->get('ecole');
+
+        if ($ecole == 'Total') {
+            $reglements = $em->getRepository('ComptaBundle:Reglement')->findAll();
+            foreach($reglements as $reglement) {
+                $value = $reglement->getMontant();
+                $caville += $value;
+                $caville = number_format((float)($caville), 2, ',', ' ');
+                $cavilleht += $value * 0.8;
+                $cavilleht = number_format((float)($cavilleht), 2, ',', ' ');
+            }
+            $tabcaville[]=array(
+                "cattc"=>$caville,
+                "caht"=>$cavilleht,
+            );
+        }
+        else {
+            $clients = $em->getRepository('ClientBundle:Client')->findByEcole($ecole);
+            foreach ($clients as $client) {
+                $reglements = $em->getRepository('ComptaBundle:Reglement')->findByIdclient($client->getId());
+                foreach($reglements as $reglement) {
+                    $value = $reglement->getMontant();
+                    $caville += $value;
+                    $caville = number_format((float)($caville), 2, ',', ' ');
+                    $cavilleht += $value * 0.8;
+                    $cavilleht = number_format((float)($cavilleht), 2, ',', ' ');
+                }
+            }
+            $tabcaville[]=array(
+                "cattc"=>$caville,
+                "caht"=>$cavilleht,
+            );
+        }
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new GetSetMethodNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($tabcaville, 'json');
+
+        $response = new Response($jsonContent);
+        return $response;
     }
 }
